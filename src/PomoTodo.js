351916d45c9f3b1ea78ac64950c440577c9d73b2
Justin Kim <naznsan@gmail.com>
2020-05-14 20:36:08 +1200

    Refactor PomoTodo with JSS

--- src/PomoTodo.js
+++ src/PomoTodo.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/styles";
 import Pomodoro from "./pomodoro/Pomodoro";
 import TodoList from "./todoList/TodoList";
-import "./PomoTodo.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+
+const styles = {
+    PomoTodo: {
+        height: "100%",
+        borderRadius: "0.25em",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "#121212",
+        padding: "0 0.5em",
+    },
+    header: {
+        width: "100 %",
+        height: "4em",
+        display: "flex",
+        alignItems: "center",
+        color: "#eeeeee",
+        "& a": {
+            marginLeft: "auto",
+            color: "#bbbbbb",
+            fontSize: "1.5em",
+        },
+    },
+};
 
 class PomoTodo extends Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div className="PomoTodo">
-                <div className="PomoTodo-header">
+            <div className={classes.PomoTodo}>
+                <div className={classes.header}>
                     <h1>PomoTodo</h1>
                     <a href="https://github.com/naznsan/pomotodo">
                         <i className="far fa-question-circle"></i>
@@ -21,4 +46,4 @@ class PomoTodo extends Component {
     }
 }
 
-export default PomoTodo;
+export default withStyles(styles)(PomoTodo);
